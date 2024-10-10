ba13567af625c7721170680bb104a4389e1fe9bb
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-10-10 22:15:02 +0100

    image_convert node: add dilate and erosion

--- trsa_lab1/image_convert.py
+++ trsa_lab1/image_convert.py
@@ -3,11 +3,13 @@ import cv2
 import os, ament_index_python
 from rclpy.node import Node
 from sensor_msgs.msg import Image
+import numpy as np
 
 from cv_bridge import CvBridge
 
-MAX_KERNEL_LENGTH = 21
-THRESHOLD1  = 20
+MAX_KERNEL_LENGTH = 2
+MAX_KERNEL_GAUSSIAN = 21
+THRESHOLD1 = 30
 THRESHOLD2 = 50
 
 class ImageConvert(Node):
@@ -22,12 +24,18 @@ class ImageConvert(Node):
     def image_convert_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         cv_image_grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+        kernel = np.ones((MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH), np.uint8)
 
-        for i in range(1, MAX_KERNEL_LENGTH, 2):
+        for i in range(1, MAX_KERNEL_GAUSSIAN, 2):
             cv_image_blur= cv2.GaussianBlur(cv_image_grey, (i, i), 0)
 
         cv_image_canny = cv2.Canny(cv_image_blur,THRESHOLD1,THRESHOLD2)
-        self.pub_image_processed.publish(self.bridge.cv2_to_imgmsg(cv_image_canny))
+        
+        # Dilate and Erode the picture
+        cv_image_dilation = cv2.dilate(cv_image_canny, kernel, iterations=2) 
+        cv_image_erosion = cv2.erode(cv_image_dilation, kernel, iterations=2) 
+        
+        self.pub_image_processed.publish(self.bridge.cv2_to_imgmsg(cv_image_erosion))
         
 
 def main(args=None):
