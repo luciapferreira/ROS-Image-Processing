6d87dd568bab6df7246d21c8c2bcdcb3e2f4f372
Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>
2024-10-15 17:32:16 +0100

    launch_file: add parameter for threshold and kernel_length values
    Signed-off-by: Gonçalo Negrier Duarte <gonegrier.duarte@gmail.com>

--- launch/launch.py
+++ launch/launch.py
@@ -5,6 +5,12 @@ from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
    camera_type = LaunchConfiguration('camera_type')
+   
+   threshold1 = LaunchConfiguration('threshold1')
+   threshold2 = LaunchConfiguration('threshold2')
+
+   max_kernel_length = LaunchConfiguration('max_kernel_length')
+   max_kernel_gaussian = LaunchConfiguration('max_kernel_gaussian')
     
    return LaunchDescription([
       
@@ -12,6 +18,28 @@ def generate_launch_description():
         'camera_type',
         default_value='test1.mov',
         description='Device ID or video file path'
+    ),
+
+    DeclareLaunchArgument(
+        'threshold1',
+        default_value='30',
+        description='OpenCV Canny Threshold1'
+    ),
+    DeclareLaunchArgument(
+        'threshold2',
+        default_value='50',
+        description='OpenCV Canny Threshold2'
+    ),
+
+    DeclareLaunchArgument(
+        'max_kernel_length',
+        default_value='2',
+        description='OpenCV Kernel Lengh for Dilate/Erode'
+    ),
+        DeclareLaunchArgument(
+        'max_kernel_gaussian',
+        default_value='21',
+        description='OpenCV Kernel Lengh for Gaussian Blur'
     ),
 
     Node(
@@ -34,6 +62,12 @@ def generate_launch_description():
         executable='image_convert',
         output='screen',
         name='image_convert',
+        parameters=[
+            {'threshold1': threshold1},
+            {'threshold2': threshold2},
+            {'max_kernel_length': max_kernel_length},
+            {'max_kernel_gaussian': max_kernel_gaussian}
+        ]
     ),
     Node(
         package='trsa_lab1',
--- trsa_lab1/image_convert.py
+++ trsa_lab1/image_convert.py
@@ -7,15 +7,22 @@ import numpy as np
 
 from cv_bridge import CvBridge
 
-MAX_KERNEL_LENGTH = 2
-MAX_KERNEL_GAUSSIAN = 21
-THRESHOLD1 = 30
-THRESHOLD2 = 50
-
 class ImageConvert(Node):
 
     def __init__(self):
         super().__init__('image_convert')
+
+        # Declare parameters
+        self.declare_parameter('threshold1', 30)
+        self.declare_parameter('threshold2', 50)
+        self.declare_parameter('max_kernel_length', 2)
+        self.declare_parameter('max_kernel_gaussian', 21)
+        
+        self.threshold1 = self.get_parameter('threshold1').get_parameter_value().integer_value
+        self.threshold2 = self.get_parameter('threshold2').get_parameter_value().integer_value
+        self.max_kernel_length = self.get_parameter('max_kernel_length').get_parameter_value().integer_value
+        self.max_kernel_gaussian = self.get_parameter('max_kernel_gaussian').get_parameter_value().integer_value
+        
 
         self.bridge = CvBridge()
         self.sub_image_convert = self.create_subscription(Image, '/camera/image_rect', self.image_convert_callback , 10)
@@ -24,12 +31,12 @@ class ImageConvert(Node):
     def image_convert_callback(self, msg):
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         cv_image_grey = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
-        kernel = np.ones((MAX_KERNEL_LENGTH, MAX_KERNEL_LENGTH), np.uint8)
+        kernel = np.ones((self.max_kernel_length, self.max_kernel_length), np.uint8)
 
-        for i in range(1, MAX_KERNEL_GAUSSIAN, 2):
+        for i in range(1, self.max_kernel_gaussian, 2):
             cv_image_blur= cv2.GaussianBlur(cv_image_grey, (i, i), 0)
 
-        cv_image_canny = cv2.Canny(cv_image_blur,THRESHOLD1,THRESHOLD2)
+        cv_image_canny = cv2.Canny(cv_image_blur,self.threshold1,self.threshold2)
         
         # Dilate and Erode the picture
         cv_image_dilation = cv2.dilate(cv_image_canny, kernel, iterations=2) 
